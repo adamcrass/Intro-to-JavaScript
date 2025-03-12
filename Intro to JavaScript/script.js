75235e12b74ae7684315a2f53bb5079e582f818f
AJ <37306837+adamcrass@users.noreply.github.com>
2025-03-12 10:47:12 -0700

    Add files via upload

---
+++ Intro to JavaScript/script.js
@@ -0,0 +1,19 @@
+var answer = 1 + 1;
+console.log(answer);
+
+console.log(" ");
+
+var MyName = "Adam";
+console.log(MyName);
+
+let UserName = prompt("What's your name?");
+
+let UserAge = prompt("How old are you?");
+
+let Name_and_Age = "You are " + UserName + ", age " + UserAge;
+
+console.log(Name_and_Age);
+
+document.write("<h1>Hey awsome person</h1>");
+
+alert("This is my first alert");
